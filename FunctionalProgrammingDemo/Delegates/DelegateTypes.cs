6fba707ce823d0d1ff9f4aa820ec2c538854d47d
Steve Bishop <steveb@gartmantech.com>
2017-05-07 23:02:51 -0700

    Added delegate section

---
+++ FunctionalProgrammingDemo/Delegates/DelegateTypes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace FunctionalProgrammingDemo.Delegates
+{
+    [TestClass]
+    public class DelegateTypes
+    {
+        public delegate void ActionDelegate(string newItem);
+        public delegate string FunctionDelegate();
+        public delegate bool PredicateDelegate(string newItem);
+
+        public void MyAction(string item) { Debug.WriteLine(item); }
+        public string MyFunction() => "MyFunction was called.";
+
+        [TestMethod]
+        public void AssigningFunctionsToDelegates()
+        {
+            ActionDelegate actionDelegate = MyAction;
+            FunctionDelegate functionDelegate = MyFunction;
+
+            actionDelegate("MyAction was called");
+            Debug.WriteLine(functionDelegate());
+        }
+
+        public Action<string> Action;
+        public Func<string> Function;
+
+        [TestMethod]
+        public void ActionAndFuncDelegates()
+        {
+            Action = MyAction;
+            Function = MyFunction;
+            Action("Action was called.");
+            Debug.WriteLine(MyFunction());
+        }
+
+        [TestMethod]
+        public void Lambdas()
+        {
+            ActionDelegate newAction = delegate (string newItem) { Debug.WriteLine(newItem); };
+            FunctionDelegate newFunction = delegate () { return "newFunction was called."; };
+        }
+    }
+}
---
+++ FunctionalProgrammingDemo/ImmutableData/Tuples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FunctionalProgrammingDemo.ImmutableData
+{
+    [TestClass]
+    public class Tuples
+    {
+        [TestMethod]
+        public void ImmutableTuples()
+        {
+            var myTuple1 = new Tuple<int, string>(100, "Name 100");
+            var myTuple2 = myTuple1;
+            Assert.AreSame(myTuple1, myTuple2);
+        }
+    }
+}
